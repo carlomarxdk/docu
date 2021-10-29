ea5c51fa1715efd5881f9183c85f3ea9f1813b10
Germans Savcisens <38817889+carlomarxdk@users.noreply.github.com>
2021-10-29 22:02:58 +0200

    CASES

--- cases_app.py
+++ cases_app.py
@@ -18,16 +18,28 @@ def get_overview():
         return redirect(url_for("output", case_id = case_id))
     return render_template("cases.html",rows = cases)
 
-@app.route('/output')
+@app.route('/output', methods = ['GET', 'POST'])
 def output():
+    if request.method == "POST":
+        return redirect(url_for("add_document"))
     value = request.args.get('case_id', None)
     con = sql.connect(DATABASE)
     con.row_factory = sql.Row
     cur = con.cursor()
     cur.execute("SELECT * from CASES WHERE ID IS %s" %value )
-    case = cur.fetchall()[0];
+    case_info = cur.fetchall()[0];
     cur.execute("SELECT * from DOCUMENTS WHERE CASE_ID IS %s" %value )
     documents = cur.fetchall();
-    return render_template("output.html", case_info  = case, documents = documents )
+    return render_template("output.html", case_info  = case_info, documents = documents )
+
+@app.route('/add_document')
+def add_document():
+    con = sql.connect(DATABASE)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("SELECT * from USERS" )
+    users = cur.fetchall();
+
+    return render_template("new_document.html", users = users)
 if __name__ == '__main__':
    app.run(debug = True)
