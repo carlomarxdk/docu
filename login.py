769bcb6b76b64ae51a4690fb102e7ce2c11529c8
Johan Chou Chen <johan@Johans-MacBook-Air.local>
2021-10-29 17:25:29 +0200

    upload button

--- login.py
+++ login.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
 # Route for handling the login page logic
@@ -11,3 +13,11 @@ def login():
         else:
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+@app.route('/up', methods=['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+       file = request.files['file']
+       filename = secure_filename(file.filename)
+       file.save(secure_filename(file.filename))
+       return 'file uploaded successfully'
