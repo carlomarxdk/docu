3b7617144c3252ebd39511d8e443b84f8e1a6a6d
Germans Savcisens <38817889+carlomarxdk@users.noreply.github.com>
2021-10-29 16:30:42 +0200

    DB Update

---
+++ db_app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import sqlite3 as sql
+
+
+DATABASE = 'database/data.db'
+
+app = Flask(__name__)
+
+@app.route('/')
+def list():
+   con = sql.connect(DATABASE)
+   con.row_factory = sql.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from people")
+   
+   rows = cur.fetchall();
+   return render_template("list.html",rows = rows)
+
+if __name__ == '__main__':
+   app.run(debug = True)
