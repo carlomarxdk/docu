c3bd336e286ee523ddbedc23991410a1d4fbc101
Germans Savcisens <38817889+carlomarxdk@users.noreply.github.com>
2021-10-29 20:53:48 +0200

    Buttons

--- cases_app.py
+++ cases_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 import sqlite3 as sql
 
 
@@ -6,20 +6,21 @@ DATABASE = 'database/data.db'
 
 app = Flask(__name__)
 
-@app.route('/')
-def get_users() -> []:
-   con = sql.connect(DATABASE)
-   con.row_factory = sql.Row
-   
-   cur = con.cursor()
-   cur.execute("SELECT * FROM CASES")
-   
-   cases = cur.fetchall();
+@app.route('/', methods = ['GET', 'POST'])
+def get_overview():
+    con = sql.connect(DATABASE)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("SELECT * FROM CASES")
+    cases = cur.fetchall();
+    if request.method == "POST":
+        case_id = request.form.get("case_id")
+        return redirect(url_for("output", case_id = case_id))
+    return render_template("cases.html",rows = cases)
 
-
-
-
-   return render_template("cases.html",rows = cases)
-
+@app.route('/output')
+def output():
+    value = request.args.get('case_id', None)
+    return render_template("output.html",case_id = value)
 if __name__ == '__main__':
    app.run(debug = True)
