42e49c1b9691eb3945864b5dd6334d3c8f442966
Germans Savcisens <38817889+carlomarxdk@users.noreply.github.com>
2021-10-30 11:28:28 +0200

    changes
    s

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, current_app
 import sqlite3 as sql
 import pdb
+import uuid
+import datetime
+from werkzeug.utils import secure_filename
+import os
+
 
 DATABASE = 'database/data.db'
 
@@ -28,12 +33,18 @@ def output():
     cur.execute("SELECT * from CASES WHERE ID IS %s" %value)
     case_info = cur.fetchall()[0];
     cur.execute("SELECT * from DOCUMENTS WHERE CASE_ID IS %s" %value)
-    documents = cur.fetchall();
+    documents = cur.fetchall();   
+
     if request.method == "POST":
         case_id = request.form.get("case_id", None)
         #return redirect(url_for("output", case_id = n))
         return redirect(url_for("add"), case_id = case_id)
-    return render_template("output.html", case_info  = case_info, documents = documents)
+    return render_template("output.html", case_info  = case_info, documents = documents, dir = current_app.root_path)
+
+@app.route('/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+    return send_from_directory(directory=uploads, filename=filename)
 
 @app.route('/add', methods = ['GET', 'POST'])
 def add():
@@ -50,12 +61,25 @@ def add():
         file_desc = request.form.get("file_description")
         file_type = request.form.get("file_type")
         file_appr = request.form.get("users")
+        file_id = str(uuid.uuid4())
+        file = request.form.get("file")
+        file_datetime = datetime.datetime.now()
+        file = request.files['file']
+        format = file.filename.split(".")[-1]
+        file_path = os.path.join(current_app.root_path, "docs", secure_filename(file_id + "." + format))
+
         ## Insert into the database
         con = sql.connect(DATABASE)
         cur = con.cursor()
-        pattern = """INSERT INTO documents(type, description, case_id, responsible) VALUES(?, ?, ?, ?);"""
-        cur.execute(pattern, (file_type, file_desc, case_id, str(file_appr)))
+        pattern = """INSERT INTO documents(id, type, description, case_id, responsible, creation_date, file_path) VALUES(?, ?, ?, ?, ?, ?, ?);"""
+        cur.execute(pattern, (file_id, file_type, file_desc, case_id, str(file_appr), file_datetime, file_path))
         con.commit()
+        pattern = """INSERT INTO approvals( type, doc_id, status, datetime, responsible) VALUES(?, ?, ?, ?, ?);"""
+        cur.execute(pattern, (file_type, file_id, "PENDING",  datetime.datetime.now(), str(file_appr)))
+        con.commit()
+        ### FILE UPLOAD
+        file.save(os.path.join("docs", secure_filename(file_id + "." + format)))
+
         ## Data for the OUTPUT.HTML
         #return render_template("new_document.html", users = users, case_id = case_id)
         #return render_template("output.html", case_info  = case_info, documents = documents)
