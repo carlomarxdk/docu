26531d6dea0ac0548bfc6bf1389dd5725c55fb48
Germans Savcisens <38817889+carlomarxdk@users.noreply.github.com>
2021-10-30 09:50:50 +0200

    WORKING

--- app.py
+++ app.py
@@ -44,19 +44,23 @@ def add():
     cur.execute("SELECT * from USERS" )
     users = cur.fetchall();
     if request.method == "POST":
+        case_id = request.form.get("case_id", None)
+
+        ## Collect data from the form
         type = request.form.get("type")
         file_name = request.form.get("file_name")
         file_appr = request.form.get("type")
+        ## Insert into the database
         con = sql.connect(DATABASE)
-        #con.row_factory = sql.Row
         cur = con.cursor()
         pattern = """INSERT INTO documents(description, type) VALUES(?, ?);"""
         cur.execute(pattern, (file_appr, file_name))
         con.commit()
-        return render_template("new_document.html", users = users, case_id = case_id, res = type)
+        ## Data for the OUTPUT.HTML
+        return redirect(from_url(".output", case_id = case_id))
+        #return render_template("output.html", case_info  = case_info, documents = documents)
+    return render_template("new_document.html", users = users, case_id = case_id)
 
 
-
-    return render_template("new_document.html", users = users, case_id = case_id, res = None)
 if __name__ == '__main__':
    app.run(debug = True)
